ed067314299940eb3dc8ecf6b921fcf3223ece2f
Aizaz Haider <aizazhaider720@gmail.com>
2023-06-12 12:15:08 +0500

    Animated Builder Example Ends Here

--- lib/screens/explicit_animations/animated_builder/animated_builder.dart
+++ lib/screens/explicit_animations/animated_builder/animated_builder.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AnimatedBuilderPage extends StatefulWidget {
@@ -10,11 +12,15 @@ class AnimatedBuilderPage extends StatefulWidget {
 class _AnimatedBuilderPageState extends State<AnimatedBuilderPage>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this);
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..repeat(reverse: true);
   }
 
   @override
@@ -25,6 +31,29 @@ class _AnimatedBuilderPageState extends State<AnimatedBuilderPage>
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Animated Builder",
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: _controller.value * 2 * pi,
+              alignment: Alignment.center,
+              child: Image.asset(
+                height: 200,
+                width: 200,
+                "assets/images/jerry.png",
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
