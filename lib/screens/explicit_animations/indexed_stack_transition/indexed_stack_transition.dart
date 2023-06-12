447d1b867aec08c9c5776106daa98c3d5a976616
Aizaz Haider <aizazhaider720@gmail.com>
2023-06-12 16:28:55 +0500

    Index Stack Ends Here

--- lib/screens/explicit_animations/indexed_stack_transition/indexed_stack_transition.dart
+++ lib/screens/explicit_animations/indexed_stack_transition/indexed_stack_transition.dart
@@ -11,11 +11,20 @@ class IndexedStackTransitionPage extends StatefulWidget {
 class _IndexedStackTransitionPageState extends State<IndexedStackTransitionPage>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<double> _scaleAnimation;
+
+  int currentIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this);
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _scaleAnimation = Tween<double>(begin: 0.3, end: 1).animate(_controller);
   }
 
   @override
@@ -26,6 +35,66 @@ class _IndexedStackTransitionPageState extends State<IndexedStackTransitionPage>
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Indexed Stack Transition",
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: IndexedStack(
+          index: currentIndex,
+          children: [
+            ScaleTransition(
+              scale: _scaleAnimation,
+              child: FadeTransition(
+                opacity: _opacityAnimation,
+                child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.green,
+                    child: Image.asset("assets/images/jerry.png")),
+              ),
+            ),
+            ScaleTransition(
+              alignment: Alignment.center,
+              scale: _scaleAnimation,
+              child: FadeTransition(
+                opacity: _opacityAnimation,
+                child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.amber,
+                    child: Image.asset("assets/images/tom.png")),
+              ),
+            ),
+            ScaleTransition(
+              alignment: Alignment.center,
+              scale: _scaleAnimation,
+              child: FadeTransition(
+                opacity: _opacityAnimation,
+                child: Container(
+                    alignment: Alignment.center,
+                    color: Colors.purple,
+                    child: Image.asset("assets/images/dog.png")),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _controller.reset();
+          _controller.forward();
+          setState(() {
+            if (currentIndex == 2) {
+              currentIndex = 0;
+            } else {
+              currentIndex++;
+            }
+          });
+        },
+        child: const Icon(Icons.arrow_forward),
+      ),
+    );
   }
 }
